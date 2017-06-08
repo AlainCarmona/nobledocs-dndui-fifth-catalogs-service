73766dc9556b653a70eed2fbacaa1c279ac00250
ALAIN CARMONA HERNANDEZ <Z841158@santander.com.mx>
2017-06-08 16:21:06 -0500

    2017-06-08: Se modifica el servicio de clases

--- src/main/resources/static/app/components/index.controller.js
+++ src/main/resources/static/app/components/index.controller.js
@@ -3,6 +3,7 @@ var app = angular.module('dnduiApp', []);
 
 app.controller('dnduiCtrl', function ($scope, $http, $sce) {
   $scope.findByName = findByName;
+  $scope.findDndClassByName = findDndClassByName;
 
   $scope.initials = [
     'A', 'B', 'C', 'D', 'E',
@@ -19,6 +20,12 @@ app.controller('dnduiCtrl', function ($scope, $http, $sce) {
     }
   });
 
+  $http.get("/dndClass/findAllNames").then(function (response) {
+    if (response.data.code === 200) {
+      $scope.dndClasses = response.data.body;
+    }
+  });
+
   function findByName(name) {
     $http.get("/spell/findByName?name=" + name).then(function (response) {
       if (response.data.code === 200) {
@@ -30,6 +37,17 @@ app.controller('dnduiCtrl', function ($scope, $http, $sce) {
         $scope.modalSpell.components = response.data.body.components;
         $scope.modalSpell.duration = response.data.body.duration;
         $scope.modalSpell.description = $sce.trustAsHtml(response.data.body.description);
+      }
+    });
+  }
+
+  function findDndClassByName(name) {
+    $http.get("/dndClass/findByName?name=" + name).then(function (response) {
+      if (response.data.code === 200) {
+        $scope.modalDndClass = {};
+        $scope.modalDndClass.name = response.data.body.name;
+        $scope.modalDndClass.featuresTable = $sce.trustAsHtml(response.data.body.featuresTable);
+        console.log(response);
       }
     });
   }
